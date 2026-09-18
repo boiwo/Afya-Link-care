5ba24bdb2962cc054ea35a3e089ecd36cf8cfe83
Boiwo Kipkurui Benard <boiwo.victor@student.moringaschool.com>
2026-09-18 22:32:02 +0300

    initial commit

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -309,7 +309,7 @@ const About = () => {
                   </div>
                 </div>
 
-                {/* QUALIFIED PROFESSIONALS */}
+                
                 <div className="flex items-start gap-4">
                   <div
                     className="
@@ -338,7 +338,6 @@ const About = () => {
                   </div>
                 </div>
 
-                {/* PATIENT SUPPORT */}
                 <div className="flex items-start gap-4">
                   <div
                     className="
@@ -372,15 +371,13 @@ const About = () => {
           </div>
         </section>
 
-        {/* ==============================
-            INTERNATIONAL NURSES DAY
-        ============================== */}
+       
         <section className="py-20 px-4 sm:px-6 lg:px-8 bg-background">
           <div className="max-w-7xl mx-auto">
 
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 items-center">
 
-              {/* INTERNATIONAL NURSES DAY IMAGE */}
+            
               <div className="w-full flex justify-center">
 
                 <div className="w-full max-w-xl">
@@ -401,7 +398,6 @@ const About = () => {
 
               </div>
 
-              {/* INTERNATIONAL NURSES DAY CONTENT */}
               <div className="space-y-7">
 
                 <div>
